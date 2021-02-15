b06f410c83fda391d1a0a2c6b2390391c57f2d85
clintonhays <clintonhays84@gmail.com>
2021-02-16 07:59:39 +0900

    add initial file structure, create homepage component

---
+++ full-stack-site/react-blog/src/pages/HomePage.js
@@ -0,0 +1,29 @@
+import React from "react";
+
+const HomePage = () => {
+	return (
+		<div>
+			<h1>Welcome to my Blog</h1>
+			<p>
+				Lorem ipsum dolor, sit amet consectetur adipisicing elit. Dolores
+				tenetur perferendis nostrum maxime cumque saepe aspernatur sapiente?
+				Perferendis, ut laudantium nemo, doloribus facilis aliquam, explicabo
+				enim voluptatum harum quasi corporis!
+			</p>
+			<p>
+				Lorem ipsum dolor, sit amet consectetur adipisicing elit. Dolores
+				tenetur perferendis nostrum maxime cumque saepe aspernatur sapiente?
+				Perferendis, ut laudantium nemo, doloribus facilis aliquam, explicabo
+				enim voluptatum harum quasi corporis!
+			</p>
+			<p>
+				Lorem ipsum dolor, sit amet consectetur adipisicing elit. Dolores
+				tenetur perferendis nostrum maxime cumque saepe aspernatur sapiente?
+				Perferendis, ut laudantium nemo, doloribus facilis aliquam, explicabo
+				enim voluptatum harum quasi corporis!
+			</p>
+		</div>
+	);
+};
+
+export default HomePage;
