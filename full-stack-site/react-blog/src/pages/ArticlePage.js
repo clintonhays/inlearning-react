6fc3f25d911a3583ad6236d2ad3869ca0d1a76ab
clintonhays <clintonhays84@gmail.com>
2021-02-18 17:09:40 +0900

    added state to articlePage component for comments & upvotes, created comment list component

--- full-stack-site/react-blog/src/pages/ArticlePage.js
+++ full-stack-site/react-blog/src/pages/ArticlePage.js
@@ -1,11 +1,24 @@
-import React from "react";
+import React, { useState, useEffect } from "react";
 import ArticlesList from "./components/ArticlesList";
 import articleContent from "./article-content";
 import NotFound from "./NotFound";
+import CommentsList from "./components/CommentsList";
 
 const ArticlePage = ({ match }) => {
 	const name = match.params.title;
 	const article = articleContent.find((article) => article.name === name);
+
+	const [articleInfo, setArticleInfo] = useState({ upvotes: 0, comments: [] });
+
+	useEffect(() => {
+		const fetchData = async () => {
+			const result = await fetch(`/api/articles/${name}`);
+			const body = await result.json();
+
+			setArticleInfo(body);
+		};
+		fetchData();
+	});
 
 	if (!article) {
 		return <NotFound />;
@@ -17,10 +30,11 @@ const ArticlePage = ({ match }) => {
 	return (
 		<>
 			<h1>{article.title}</h1>
+			<p>This post has been upvoted {articleInfo.upvotes} times</p>
 			{article.content.map((p, key) => (
 				<p key={key}>{p}</p>
 			))}
-
+			<CommentsList comments={articleInfo.comments} />
 			<h3>Other Articles:</h3>
 			<ArticlesList articles={otherArticles} />
 		</>
